514ebed3e2eae2680a678c94dcddb46c827f9ae3
Dennis Sergeev <dennis.sergeev@gmail.com>
2021-06-24 11:01:56 +0100

    add triage_run and proc_um_output

---
+++ pouch/proc_um_output.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""Functions for post-processing UM output."""
+
+# Commonly used standard library tools
+import re
+import warnings
+
+# Scientific stack
+import iris
+
+# My packages and local scripts
+from aeolus.coord import (
+    add_planet_calendar,
+    regrid_3d,
+    replace_z_coord,
+    roll_cube_pm180,
+    ensure_bounds,
+    interp_to_cube_time,
+)
+from aeolus.model import um
+from aeolus.subset import CM_INST_CONSTR, CM_MEAN_CONSTR, DimConstr, unique_cubes
+
+from .path import full_path_glob
+
+
+GLM_MODEL_TIMESTEP = 86400 / 72
+INCR_CONSTR = iris.AttributeConstraint(STASH=lambda x: x.item in [181, 182, 233])
+GLM_RUNID = r"umglaa"  # file prefix
+GLM_FILE_REGEX = GLM_RUNID + r".p[b,c,d,e]{1}[0]{6}(?P<timestamp>[0-9]{2,6})_00"
+
+
+def process_cubes(
+    cubelist,
+    timestep=GLM_MODEL_TIMESTEP,
+    ref_cube_constr=um.sh,
+    extract_incr=True,
+    extract_mean=True,
+    regrid_multi_lev=True,
+    roll_pm180=True,
+    add_calendar=False,
+    calendar=None,
+    planet="earth",
+    remove_duplicates=True,
+    use_varpack=False,
+    varpack=None,
+    interp_time=True,
+    model=um,
+):
+    """Post-process data for easier analysis."""
+    DC = DimConstr(model=model)
+
+    if remove_duplicates:
+        cubelist = unique_cubes(cubelist)
+
+    cm_constr = iris.Constraint()
+    if extract_mean:
+        cm_constr &= CM_MEAN_CONSTR
+    else:
+        cm_constr &= CM_INST_CONSTR
+    cubelist = cubelist.extract(cm_constr)
+
+    cubes = iris.cube.CubeList()
+
+    # First, extract all multi-level fields
+    if use_varpack:
+        cubes += cubelist.extract(varpack["multi_level"])
+
+        # Increments
+        if extract_incr:
+            cubes += cubelist.extract(INCR_CONSTR)
+    else:
+        cubes = cubelist.extract(DC.strict.tmyx)
+
+    if regrid_multi_lev:
+        # Interpolation & regridding to common grid
+        ref_cube = cubes.extract_cube(ref_cube_constr)
+        ref_cube = replace_z_coord(ref_cube, model=model)
+
+        # Interpolate to common levels
+        cubes = iris.cube.CubeList(
+            [
+                regrid_3d(replace_z_coord(cube, model=model), ref_cube, model=model)
+                for cube in cubes
+            ]
+        )
+    # Fix units of increments
+    for cube in cubes:
+        if cube.attributes["STASH"].item in [181, 233]:
+            incr_unit = "K"
+        elif cube.attributes["STASH"].item in [182]:
+            incr_unit = "kg kg^-1"
+        if cube.attributes["STASH"].item == 233:
+            cube.units = f"{incr_unit} s^-1"
+        elif cube.attributes["STASH"].item in [181, 182]:
+            cube.units = f"{1/timestep} {incr_unit} s^-1"
+            cube.convert_units(f"{incr_unit} s^-1")
+
+    # Add all single-level cubes
+    if use_varpack:
+        cubes += cubelist.extract(varpack["single_level"])
+    else:
+        cubes += cubelist.extract(DC.strict.tyx)
+
+    # Roll cubes to +/- 180 degrees in longitude for easier analysis
+    if roll_pm180:
+        rolled_cubes = iris.cube.CubeList()
+        for cube in cubes:
+            r_c = roll_cube_pm180(cube)
+            ensure_bounds(r_c)
+            rolled_cubes.append(r_c)
+    else:
+        rolled_cubes = cubes
+
+    if interp_time:
+        ref_cube = rolled_cubes.extract_cube(model.t_sfc)  # TODO: make an arg
+        final = iris.cube.CubeList()
+        for cube in rolled_cubes:
+            final.append(interp_to_cube_time(cube, ref_cube, model=model))
+    else:
+        final = rolled_cubes
+
+    if add_calendar:
+        try:
+            cal = calendar[planet]
+            for cube in final:
+                add_planet_calendar(
+                    cube,
+                    model.t,
+                    days_in_year=cal["year"],
+                    days_in_month=cal["month"],
+                    days_in_day=cal["day"],
+                    planet=planet,
+                )
+        except KeyError:
+            warnings.warn(f"Calendar for {planet =} not found.")
+    return final
+
+
+def get_filename_list(
+    path_to_dir,
+    glob_pattern=f"{GLM_RUNID}*",
+    ts_start=0,
+    ts_end=-1,
+    every=1,
+    regex=GLM_FILE_REGEX,
+    regex_key="timestamp",
+    sort=True,
+):
+    """Get a list of files with timestamps greater or equal than start in a directory."""
+    glob_gen = full_path_glob(path_to_dir / glob_pattern)
+    fnames = []
+    tstamps = {}
+    for fpath in glob_gen:
+        match = re.match(regex, fpath.name)
+        if match:
+            ts_num = int(match[regex_key])
+            if (ts_num >= ts_start) and (ts_num % every == 0):
+                if (ts_end == -1) or (ts_num <= ts_end):
+                    fnames.append(fpath)
+                    tstamps[fpath] = ts_num
+    if sort:
+        fnames = sorted(fnames, key=lambda x: tstamps[x])
+    return fnames
---
+++ pouch/triage_run.py
@@ -0,0 +1,432 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""Triage a standard UM run."""
+# Standard library tools
+import argparse
+from pathlib import Path
+from time import time
+import warnings
+
+# Sci
+import iris
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnchoredText
+import numpy as np
+
+# My packages and local scripts
+from aeolus.calc import horiz_wind_cmpnts, spatial, toa_net_energy, zonal_mean
+from aeolus.coord import get_cube_rel_days, interp_to_cube_time, isel, roll_cube_pm180
+from aeolus.io import load_data
+from aeolus.model import um
+from aeolus.plot import tex2cf_units
+
+from .proc_um_output import get_filename_list
+from .log import create_logger
+
+# Global definitions and styles
+warnings.filterwarnings("ignore")
+SCRIPT = Path(__file__).name
+plt.rcParams["mathtext.default"] = "regular"
+plt.rcParams["savefig.bbox"] = "tight"
+GLM_FILE_REGEX_OPTS = [
+    r"umglaa.p[a]{1}(?P<timestamp>[0-9]{6})",
+    r"umglaa.p[b,c,d]{1}[0]{6}(?P<timestamp>[0-9]{2,6})_00",
+]
+
+
+def _get_uv_and_scalar(cubelist, scalar_name, z_idx=20, model=um):
+    """
+    Extract u and v winds and a scalar field and average them in time.
+
+    Parameters
+    ----------
+    cubelist: iris.cube.CubeList
+        List of cubes with horizontal wind components
+    scalar_name: str
+        Name of the scalar variable to extract.
+    z_idx: int, optional
+        Index in the vertical dimension.
+    model: aeolus.model.Model, optional
+        Model class with relevant variable names.
+
+    Returns
+    -------
+    u, v, scalar: iris.cube.Cube
+        Cubes of wind components and a scalar variable.
+    """
+    cl = isel(cubelist, model.z, z_idx)
+    u, v = horiz_wind_cmpnts(cl, model=model)
+    s = cl.extract_cube(scalar_name)
+    cubes = []
+    for cube in [u, v, s]:
+        cube = cube.collapsed(model.t, iris.analysis.MEAN)
+        cube.coord(model.x).bounds = None
+        cube = roll_cube_pm180(cube, model=model)
+        cubes.append(cube)
+    return cubes
+
+
+def parse_args(args=None):
+    """Argument parser."""
+    ap = argparse.ArgumentParser(
+        SCRIPT,
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        epilog=f"""Usage:
+./{SCRIPT} ~/path/to/inp/dir/ -p trap1e -s 1000 -r hab1 -o ~/plots/triage
+""",
+    )
+
+    ap.add_argument("inpdir", type=str, help="Input directory")
+    ap.add_argument(
+        "-o",
+        "--outdir",
+        required=True,
+        type=str,
+        help="Directory to save plots",
+    )
+    ap.add_argument(
+        "--filemask",
+        type=int,
+        required=False,
+        default=0,
+        help=f"File mask option {[*enumerate(GLM_FILE_REGEX_OPTS)]}",
+    )
+    ap.add_argument(
+        "-c",
+        "--constdir",
+        required=True,
+        type=str,
+        help="Directory with constants for aeolus",
+    )
+    ap.add_argument(
+        "-p",
+        "--planet",
+        type=str,
+        required=False,
+        default="earth",
+        help="Planet configuration",
+    )
+    ap.add_argument(
+        "-s",
+        "--startday",
+        type=int,
+        default=0,
+        help="Load files with timestamp >= this",
+    )
+    ap.add_argument(
+        "-l",
+        "--label",
+        type=str,
+        help="Custom run label",
+    )
+    ap.add_argument(
+        "--noshow",
+        action="store_true",
+        default=False,
+        help="Do not show the image and only save it to file",
+    )
+    return ap.parse_args(args)
+
+
+def plotter(cubelist, label, outdir, show=True):
+    """Create a plot from the given cubelist."""
+    vrbl2plot = {
+        "toa_net": {
+            "func": lambda cl: spatial(toa_net_energy(cl), "mean"),
+            "tex_units": "$W$ $m^{-2}$",
+            "title": "TOA radiation balance",
+        },
+        "p_sfc": {
+            "func": lambda cl: spatial(cl.extract_cube(um.p_sfc), "mean"),
+            "tex_units": "$hPa$",
+            "title": "Surface pressure",
+        },
+        "t_sfc": {
+            "func": lambda cl: [
+                spatial(cl.extract_cube(um.t_sfc), i) for i in ["min", "mean", "max"]
+            ],
+            "tex_units": ["$K$"] * 3,
+            "title": "Surface temperature",
+        },
+        "u": {
+            "func": lambda cl: [
+                spatial(cl.extract_cube(um.u), i).collapsed(
+                    um.z, getattr(iris.analysis, i.upper())
+                )
+                for i in ["min", "mean", "max"]
+            ],
+            "tex_units": ["$m$ $s^{-1}$"] * 3,
+            "title": "Zonal wind",
+        },
+        "temp": {
+            "func": lambda cl: [
+                spatial(isel(cl.extract_cube(um.temp), um.z, idx), "mean")
+                for idx in [1, 5, 10, 15, 20, 25, 30]
+            ],
+            "tex_units": ["$K$"] * 7,
+            "title": "Potential temperature",
+        },
+        "sh": {
+            "func": lambda cl: [
+                spatial(isel(cl.extract_cube(um.sh), um.z, idx), "mean")
+                for idx in [1, 5, 10, 15, 20, 25, 30]
+            ],
+            "tex_units": ["$kg$ $kg^{-1}$"] * 7,
+            "title": "Specific humidity",
+        },
+        "map_low_atm": {
+            "func": lambda cl: _get_uv_and_scalar(cl, um.t_sfc, z_idx=0),
+            "tex_units": ["$m$ $s^{-1}$", "$m$ $s^{-1}$", "$K$"],
+            "title": "Surface temperature and wind vectors",
+        },
+        "map_up_atm": {
+            "func": lambda cl: _get_uv_and_scalar(cl, um.toa_olr, z_idx=20),
+            "tex_units": ["$m$ $s^{-1}$", "$m$ $s^{-1}$", "$W$ $m^{-2}$"],
+            "title": "TOA OLR and wind vectors",
+        },
+        "vcross": {
+            "func": lambda cl: [
+                zonal_mean(cl.extract_cube(i).collapsed(um.t, iris.analysis.MEAN))
+                for i in [um.u, um.temp]
+            ],
+            "tex_units": ["$m$ $s^{-1}$", "$K$"],
+            "title": "Zonal mean zonal wind and potential temperature",
+        },
+    }
+    # Equalise time coordinate
+    cl = iris.cube.CubeList()
+    for cube in cubelist:
+        cl.append(interp_to_cube_time(cube, cubelist.extract_cube(um.t_sfc)))
+    cubelist = cl
+
+    # Compute the specified diagnostics
+    for vrbl_dict in vrbl2plot.values():
+        vrbl_dict["cubes"] = vrbl_dict["func"](cubelist)
+        if isinstance(vrbl_dict["cubes"], iris.cube.Cube):
+            vrbl_dict["cubes"].convert_units(tex2cf_units(vrbl_dict["tex_units"]))
+        else:
+            for cube, t_u in zip(vrbl_dict["cubes"], vrbl_dict["tex_units"]):
+                cube.convert_units(tex2cf_units(t_u))
+
+    # Make the plot
+    nrows = 3
+    ncols = 3
+    yskip = 5
+    xskip = 4
+    kw_grid = dict(color="#AAAAAA", linestyle="-", linewidth=0.5)
+
+    fig = plt.figure(figsize=(nrows * 6, ncols * 4))
+
+    plt_lab = "toa_net"
+    ax = fig.add_subplot(nrows, ncols, 1, label=plt_lab)
+    cube = vrbl2plot[plt_lab]["cubes"]
+    ax.plot(get_cube_rel_days(cube), cube.data, marker=".")
+    ax.set_xlabel("Time [day]")
+    ax.set_ylabel(vrbl2plot[plt_lab]["tex_units"])
+    ax.grid(**kw_grid)
+
+    plt_lab = "p_sfc"
+    ax = fig.add_subplot(nrows, ncols, 4, label=plt_lab)
+    cube = vrbl2plot[plt_lab]["cubes"]
+    ax.plot(get_cube_rel_days(cube), cube.data, marker=".")
+    ax.set_xlabel("Time [day]")
+    ax.set_ylabel(vrbl2plot[plt_lab]["tex_units"])
+    ax.grid(**kw_grid)
+
+    plt_lab = "t_sfc"
+    ax = fig.add_subplot(nrows, ncols, 2, label=plt_lab)
+    cubes = vrbl2plot[plt_lab]["cubes"]
+    ax.fill_between(
+        get_cube_rel_days(cubes[0]), cubes[0].data, cubes[2].data, alpha=0.5
+    )
+    ax.plot(get_cube_rel_days(cubes[1]), cubes[1].data, marker=".")
+    ax.set_xlabel("Time [day]")
+    ax.set_ylabel(vrbl2plot[plt_lab]["tex_units"][0])
+    ax.grid(**kw_grid)
+
+    plt_lab = "u"
+    ax = fig.add_subplot(nrows, ncols, 5, label=plt_lab)
+    cubes = vrbl2plot[plt_lab]["cubes"]
+    ax.fill_between(
+        get_cube_rel_days(cubes[0]), cubes[0].data, cubes[2].data, alpha=0.5
+    )
+    ax.plot(get_cube_rel_days(cubes[1]), cubes[1].data, marker=".")
+    ax.set_xlabel("Time [day]")
+    ax.set_ylabel(vrbl2plot[plt_lab]["tex_units"][0])
+    ax.grid(**kw_grid)
+
+    plt_lab = "temp"
+    ax = fig.add_subplot(nrows, ncols, 3, label=plt_lab)
+    cubes = vrbl2plot[plt_lab]["cubes"]
+    for cube in cubes:
+        leg_label = f"{cube.coord(um.z).points[0]:.0f} m"
+        ax.plot(
+            get_cube_rel_days(cube),
+            cube.data,
+            label=leg_label,
+        )
+    ax.legend(title="Levels")
+    ax.set_xlabel("Time [day]")
+    ax.set_ylabel(vrbl2plot[plt_lab]["tex_units"][0])
+    ax.grid(**kw_grid)
+
+    plt_lab = "sh"
+    ax = fig.add_subplot(nrows, ncols, 6, label=plt_lab)
+    cubes = vrbl2plot[plt_lab]["cubes"]
+    for cube in cubes:
+        leg_label = f"{cube.coord(um.z).points[0]:.0f} m"
+        ax.plot(
+            get_cube_rel_days(cube),
+            cube.data,
+            label=leg_label,
+        )
+    ax.legend(title="Levels")
+    ax.set_xlabel("Time [day]")
+    ax.set_ylabel(vrbl2plot[plt_lab]["tex_units"][0])
+    ax.set_yscale("log")
+    ax.grid(**kw_grid)
+
+    plt_lab = "map_low_atm"
+    ax = fig.add_subplot(nrows, ncols, 7, label=plt_lab)
+    cubes = vrbl2plot[plt_lab]["cubes"]
+    cntrf = ax.contourf(
+        cubes[-1].coord(um.x).points,
+        cubes[-1].coord(um.y).points,
+        cubes[-1].data,
+        cmap="inferno",
+    )
+    cb = fig.colorbar(cntrf, ax=ax, orientation="horizontal", pad=0.2)
+    ax.quiver(
+        cubes[0].coord(um.x).points[::xskip],
+        cubes[0].coord(um.y).points[::yskip],
+        cubes[0].data[::yskip, ::xskip],
+        cubes[1].data[::yskip, ::xskip],
+        color="k",
+    )
+    ax.set_xlim(cubes[0].coord(um.x).points[0], cubes[0].coord(um.x).points[-1])
+    ax.set_ylim(cubes[0].coord(um.y).points[0], cubes[0].coord(um.y).points[-1])
+    cb.ax.set_xlabel(vrbl2plot[plt_lab]["tex_units"][-1])
+    ax.set_xlabel(r"Longitude [$^\circ$]")
+    ax.set_ylabel(r"Latitude [$^\circ$]")
+    ax.add_artist(
+        AnchoredText(f"Wind at {cubes[0].coord(um.z).points[0]:.0f} m", loc=1)
+    )
+    ax.grid(**kw_grid)
+
+    plt_lab = "map_up_atm"
+    ax = fig.add_subplot(nrows, ncols, 8, label=plt_lab)
+    cubes = vrbl2plot[plt_lab]["cubes"]
+    cntrf = ax.contourf(
+        cubes[-1].coord(um.x).points,
+        cubes[-1].coord(um.y).points,
+        cubes[-1].data,
+        cmap="cubehelix",
+    )
+    cb = fig.colorbar(cntrf, ax=ax, orientation="horizontal", pad=0.2)
+    ax.quiver(
+        cubes[0].coord(um.x).points[::xskip],
+        cubes[0].coord(um.y).points[::yskip],
+        cubes[0].data[::yskip, ::xskip],
+        cubes[1].data[::yskip, ::xskip],
+        color="k",
+    )
+    ax.set_xlim(cubes[0].coord(um.x).points[0], cubes[0].coord(um.x).points[-1])
+    ax.set_ylim(cubes[0].coord(um.y).points[0], cubes[0].coord(um.y).points[-1])
+    cb.ax.set_xlabel(vrbl2plot[plt_lab]["tex_units"][-1])
+    ax.set_xlabel(r"Longitude [$^\circ$]")
+    ax.set_ylabel(r"Latitude [$^\circ$]")
+    ax.add_artist(
+        AnchoredText(f"Wind at {cubes[0].coord(um.z).points[0]:.0f} m", loc=1)
+    )
+    ax.grid(**kw_grid)
+
+    plt_lab = "vcross"
+    ax = fig.add_subplot(nrows, ncols, 9, label=plt_lab)
+    cubes = vrbl2plot[plt_lab]["cubes"]
+    cntrf = ax.contourf(
+        cubes[0].coord(um.y).points,
+        cubes[0].coord(um.z).points,
+        cubes[0].data,
+        cmap="viridis",
+    )
+    cntr = ax.contour(
+        cubes[1].coord(um.y).points,
+        cubes[1].coord(um.z).points,
+        cubes[1].data,
+        cmap="inferno",
+        levels=np.concatenate([np.arange(0, 400, 20), np.arange(400, 2000, 200)]),
+    )
+    ax.clabel(cntr, fmt="%.0f")
+    cb = fig.colorbar(cntrf, ax=ax, orientation="horizontal", pad=0.2)
+    cb.ax.set_xlabel(vrbl2plot[plt_lab]["tex_units"][0])
+    ax.set_xlabel(r"Latitude [$^\circ$]")
+    ax.set_ylabel("Height [$m$]")
+    ax.set_ylim(cubes[0].coord(um.z).points[0], cubes[0].coord(um.z).points[-1])
+
+    axdict = {ax.get_label(): ax for ax in fig.axes if ax.get_label() != "<colorbar>"}
+
+    for plt_lab, ax in axdict.items():
+        ax.set_title(vrbl2plot[plt_lab]["title"], loc="left", fontsize=8)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+    fig.suptitle(f"Experiment: {label}", y=0.925)
+
+    fname_out = outdir / f"triage_{label}.png"
+    fig.savefig(fname_out, dpi=200)
+    if show:
+        plt.show()
+    return fname_out
+
+
+def main(args=None):
+    """Main entry point of the script."""
+    t0 = time()
+    L = create_logger(Path(__file__))
+    # Parse command-line arguments
+    args = parse_args(args)
+    planet = args.planet
+    L.info(f"{planet=}")
+
+    # Input directory
+    inpdir = Path(args.inpdir)
+    L.info(f"{inpdir=}")
+
+    # GLM regex option
+    glm_regex = GLM_FILE_REGEX_OPTS[args.filemask]
+
+    # Make a list of files matching the file mask and the start day threshold
+    fnames = get_filename_list(inpdir, ts_start=args.startday, every=1, regex=glm_regex)
+    if len(fnames) == 0:
+        L.critical("No files found!")
+        return
+    L.info(f"fnames({len(fnames)}) = {fnames[0]} ... {fnames[-1]}")
+
+    # Output image file name
+    if args.label:
+        run_label = args.label
+    else:
+        # Determine run label automatically
+        pth = Path(fnames[0])
+        try:
+            run_label = pth.parts[pth.parts.index("cylc-run") + 1]
+        except ValueError:
+            run_label = "unknown"
+            L.info(f"'cylc-run' is not in the {pth}, run_label is set to {run_label}")
+
+    # Load data from selected files
+    cl = load_data(fnames)  # , name=run_label, planet=planet, const_dir=args.constdir)
+    if len(cl) == 0:
+        L.critical("Files are empty!")
+        return
+
+    outdir = args.outdir
+    outdir.mkdir(parents=True, exist_ok=True)
+
+    # Process data and make a plot
+    fname_out = plotter(cl, run_label, outdir=outdir, show=(not args.noshow))
+    L.success(f"Image saved to {fname_out}")
+    L.info(f"Execution time: {time() - t0:.1f}s")
+
+
+if __name__ == "__main__":
+    main()
